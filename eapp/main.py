4e78529416bc6c18d5fe512828d88a0d7ec3ef54
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-10-18 17:58:43 +0700

    udpate

--- eapp/main.py
+++ eapp/main.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 import sys
-
-ROOT_DIR = Path(__file__).resolve().parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT_DIR))
 
 from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', msg='Welcome to App!')
 
 
+if __name__ == '__main__':
+    app.run()
